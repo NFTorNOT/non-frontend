fb010f200acd151b11a5092af1ec767b1222327a
Rohit <me@rjha.dev>
2022-12-03 13:03:54 +0000

    feat: vote page  css and html

--- src/components/VoteImage/index.js
+++ src/components/VoteImage/index.js
@@ -1,3 +1,65 @@
+import styles from "./Main.module.scss";
+
 export default function VoteImage() {
-    return <div>Vote Image</div>
+  const wordOfTheDay = "Light";
+  const imageurl = "https://static.nftornot.com/img.png";
+  const cross = "https://static.nftornot.com/Frame+24.png";
+  const right = "https://static.nftornot.com/hot.png";
+  var sectionStyle = {
+    backgroundImage: `url(${imageurl})`,
+  };
+  const promt = "The Forgotten Prince of The Kingdom of Eternal Sunlight";
+  const id = "@harshit.lens";
+  const polygon = "0x34...2745";
+  const ipfs = "0x34...2745";
+
+  return (
+    <div className={styles.wrapper}>
+      <img alt="wrong" src={cross} className={styles.bb} />
+      <div className={styles.secondTab}>
+        <div className={styles.yellow}>Word of the day</div>
+        <center>
+          <div className={styles.wordOfDay}>"{wordOfTheDay}"</div>
+        </center>
+
+        <div
+          style={{
+            display: "grid",
+          }}
+        >
+          <div className={styles.generatedImagePrompts} style={sectionStyle}>
+            <div className={styles.nftContractInfo}>
+              <div className={styles.nfttext}>NFT contract information</div>
+              <div className={styles.polygon}>
+                <img
+                  src="https://static.nftornot.com/polygon-matic-logo+2.png"
+                  alt="polygon"
+                ></img>
+                <span>Polygon transaction:</span>
+                <span>{polygon}</span>
+              </div>
+
+              <div className={styles.polygon}>
+                <img
+                  src="https://static.nftornot.com/Ipfs-logo-1024-ice-text+1.png"
+                  alt="ipfs"
+                ></img>
+                <span>IPFS metadata: </span>
+                <span>{ipfs}</span>
+              </div>
+            </div>
+            <div className={styles.end}>
+              <div className={styles.promt}>{promt}</div>
+              <div className={styles.nftInfo}>
+                <div className={styles.id}>{id}</div>
+                <button className={styles.nftButton}>NFT contract info</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <img alt="right" src={right} className={styles.bb} />
+    </div>
+  );
 }
