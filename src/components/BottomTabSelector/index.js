851bd57f642a2a1c9b3de5e1bb7ce76f529465de
thahir-reddy <thahir@plgworks.com>
2022-12-06 14:01:07 +0530

    hover fixes

--- src/components/BottomTabSelector/index.js
+++ src/components/BottomTabSelector/index.js
@@ -1,9 +1,9 @@
 import { useBottomTab } from "../../context/BottomTabContext";
 import { TabItems } from "../Main/TabItems";
-import styles from "./BottomTabSelector.module.css";
+import styles from "./BottomTabSelector.module.scss";
 
 export default function BottomTabSelector() {
-  const {currentTab, onTabChange} = useBottomTab()
+  const { currentTab, onTabChange } = useBottomTab();
   return (
     <div className={styles.container}>
       {Object.values(TabItems).map((tab) => {
