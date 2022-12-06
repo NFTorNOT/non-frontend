fe3067aeaeeafcd1c7ec3d3730a14eac6977a4ff
thahir-reddy <thahir@plgworks.com>
2022-12-06 13:45:44 +0530

    fixes

--- src/components/NONLogo/index.js
+++ src/components/NONLogo/index.js
@@ -3,11 +3,12 @@ import styles from "./NONLogo.module.css";
 export default function NONLogo() {
   return (
     <div className={styles.container}>
-      <span className={styles.NFT}>NFT</span>
-      &nbsp;
-      <div className={styles.orContainer}><span className={styles.or}>or</span></div>
-      &nbsp;
-      <span className={styles.Not}>Not</span>
+      <img
+        src="https://static.plgworks.com/assets/images/hon/hon-logo.png"
+        alt="HoN Logo"
+        width="182.6"
+        height="44.33"
+      />
     </div>
   );
 }
