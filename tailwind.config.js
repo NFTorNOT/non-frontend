6e8444fdbbaaa722f4babc8e281440d604e304c2
dhruvtailor7 <dhruv@plgworks.com>
2022-12-08 15:45:30 +0530

    tailwind configuration changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,41 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        satoshi: ["satoshi", "sans-serif"],
-      },
+const plugin = require("tailwindcss/plugin");
+const buttons = require("./theme/buttons.json");
+
+
+const content = ["./src/**/*.{js,jsx,ts,tsx}"];
+const theme = {
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
+  container: {
+    center: true,
+    padding: "15px",
+  },
+  borderRadius: {
+    4: "4px",
+  },
+  extend: {
+    fontFamily: {
+      satoshi: ["satoshi", "sans-serif"],
     },
   },
-  plugins: [],
 };
+
+const plugins = [
+  plugin(function ({ addComponents }) {
+    addComponents(buttons);
+  }),
+];
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content,
+  theme,
+  plugins
+}
+
+
+
