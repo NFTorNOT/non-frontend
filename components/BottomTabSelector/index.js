8356b1564122647038edb21a6ad5b27a32bc7cab
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2022-12-13 17:45:06 +0530

    Social sharing UI done

--- components/BottomTabSelector/index.js
+++ components/BottomTabSelector/index.js
@@ -1,27 +1,39 @@
 import { useBottomTab } from "../../context/BottomTabContext";
 import { TabItems } from "../Main/TabItems";
 import styles from "./BottomTabSelector.module.scss";
+import TwitterIcon from './svg/TwitterIcon';
+import TelegramIcon from './svg/TelegramIcon';
+import QuestionMarkIcon from './svg/QuestionMarkIcon';
 
 export default function BottomTabSelector() {
   const { currentTab, onTabChange } = useBottomTab();
   return (
-    <div className={styles.container}>
-      {Object.values(TabItems).map((tab) => {
-        const isSelected = tab.id === currentTab.id;
-        return (
-          <div
-            key={tab.id}
-            onClick={() => onTabChange(tab)}
-            id={tab.id}
-            className={`${styles.tabContainer} ${
-              isSelected ? styles.selectedTab : {}
-            }`}
-            title={tab.tabName}
-          >
-            {tab.tabName}
-          </div>
-        );
-      })}
+    <div className='flex'>
+      <div className='basis-2/12  flex items-center'>
+        <QuestionMarkIcon />
+        <span className='text-[16px] text-[#fff] pl-[10px]'>How does this work?</span>
+      </div>
+      <div className={`${styles.container} basis-8/12`}>
+        {Object.values(TabItems).map((tab) => {
+          const isSelected = tab.id === currentTab.id;
+          return (
+            <div
+              key={tab.id}
+              onClick={() => onTabChange(tab)}
+              id={tab.id}
+              className={`${styles.tabContainer} ${isSelected ? styles.selectedTab : {}
+                }`}
+              title={tab.tabName}
+            >
+              {tab.tabName}
+            </div>
+          );
+        })}
+      </div>
+      <div className='basis-2/12 flex items-center justify-end gap-[20px]'>
+        <TwitterIcon/>
+        <TelegramIcon />
+      </div>
     </div>
   );
 }
