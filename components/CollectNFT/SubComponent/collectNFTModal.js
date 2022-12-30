3eb262d8845a0b25dbb261239f724c7723ce4958
dakshbhardwaj <dakshbhardwaj2@gmail.com>
2022-12-30 14:07:00 +0530

    added error modals on collect pafe

--- components/CollectNFT/SubComponent/collectNFTModal.js
+++ components/CollectNFT/SubComponent/collectNFTModal.js
@@ -9,6 +9,7 @@ import { ERROR_TYPES } from "../../../utils/Constants";
 import PublicationApi from "../../../graphql/PublicationApi";
 import { axiosInstance } from "../../../AxiosInstance";
 import LensHelper from "../../../utils/LensHelper";
+import AlertIcon from "./SVG/AlertIcon";
 
 function CollectNFTModal({ shown, close, modalData }) {
   const [isLoading, setIsLoading] = useState(false);
@@ -148,84 +149,117 @@ function CollectNFTModal({ shown, close, modalData }) {
           e.stopPropagation();
         }}
       >
-        <div className="flex justify-between items-center">
-          <div className="flex items-center">
-            <span>
-              <Collect />
-            </span>
-            <span className="ml-[8px] font-bold text-[16px] leading-[26px] text-[#ffffff]">
-              Fee Collect
-            </span>
-          </div>
-          <button onClick={close}>
-            <Close />
-          </button>
-        </div>
-        <div className={`${styles.nftTitle} mt-[8px]`}>{modalData?.title}</div>
-        <div className={`${styles.nftOwner} mt-[8px]`}>
-          Proceeds from the Collect will go to{" "}
-          <span className="text-[#ffffff]">{modalData?.handle}</span>
-        </div>
-        <div className={`${styles.nftFee} mt-[14px] flex items-center`}>
-          <span>
-            <Image
-              src="https://static.plgworks.com/assets/images/non/wmatic-icon.png"
-              alt="Lens Icon"
-              width="26"
-              height="26"
-            />
-          </span>
-          <span className="ml-[8px]">1 WMATIC</span>
-        </div>
-        <div className={`${styles.collectorCount} flex items-center`}>
-          <span>
-            <Image
-              src="https://static.plgworks.com/assets/images/non/collectors.png"
-              alt="Lens Icon"
-              width="26"
-              height="26"
-            />
-          </span>
-          <span className="ml-[12px]">{totalCollects} Collectors</span>
-        </div>
-        {modalData?.hasCollected || isNftCollectedByMe.current ? (
-          <button
-            className={`${styles.alreadyCollectedButton} flex items-center justify-center py-[7px]`}
+        {apierror ? (
+          <div
+            className={`p-[24px] gap-[14px] w-[464px] absolute rounded-[8px] backdrop-blur-[60px] ${styles.collectModal}`}
           >
-            <span>
-              <Collect />
-            </span>
-            <span className="font-bold text-[16px] leading-[26px] ml-[8px]">
-              You have already collected this
-            </span>
-          </button>
-        ) : (
-          <button
-            className={`${styles.collectButton} flex items-center justify-center py-[7px] mt-[20px]`}
-            onClick={() => collectPost()}
-          >
-            <span>
-              <Collect />
-            </span>
-            {isLoading ? (
-              <span className="pl-[11px]">Collecting...</span>
-            ) : isNftCollected ? (
-              <span className="pl-[11px]">Collected</span>
-            ) : (
-              <span className="pl-[11px]">Collect Now</span>
-            )}
-          </button>
-        )}
-
-        {isError && apierror ? (
-          <div className="flex items-center justify-center mt-5">
-            <span>user rejected signing</span>
+            <div className="flex gap-[8px] items-center">
+              <AlertIcon />
+              <span className="w-[360px] text-[20px] leading-[160%] font-bold text-[##FFFFFF]">
+                Couldn’t Collect post
+              </span>
+            </div>
+            <div
+              className={`font-medium text-[16px] leading-[160%] pb-[106px] text-[#rgba(255, 255, 255, 0.6)]`}
+            >
+              {isError && apierror ? (
+                <div className="flex items-center justify-center mt-5">
+                  <span>user rejected signing</span>
+                </div>
+              ) : null}
+              {!isError && apierror ? (
+                <div className="flex items-center justify-center mt-5">
+                  <span>{apierror}</span>
+                </div>
+              ) : null}
+            </div>
+            <div
+              className={`w-full min-w-[156px] h-[40px] rounded-[40px] flex items-center justify-center z-10 cursor-pointer gap-[8px] ${styles.Buttonbg}`}
+              onClick={() => {
+                close();
+              }}
+            >
+              <div
+                className={`not-italic font-bold text-[16px] leading-[160%] text-center text-[#FFFFFF] py-[7px]${styles.ButtonText}`}
+              >
+                Ok. Got it.
+              </div>
+            </div>
           </div>
         ) : null}
-        {!isError && apierror ? (
-          <div className="flex items-center justify-center mt-5">
-            <span>{apierror}</span>
-          </div>
+        {!apierror ? (
+          <>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                <span>
+                  <Collect />
+                </span>
+                <span className="ml-[8px] font-bold text-[16px] leading-[26px] text-[#ffffff]">
+                  Fee Collect
+                </span>
+              </div>
+              <button onClick={close}>
+                <Close />
+              </button>
+            </div>
+            <div className={`${styles.nftTitle} mt-[8px]`}>
+              {modalData?.title}
+            </div>
+            <div className={`${styles.nftOwner} mt-[8px]`}>
+              Proceeds from the Collect will go to{" "}
+              <span className="text-[#ffffff]">{modalData?.handle}</span>
+            </div>
+            <div className={`${styles.nftFee} mt-[14px] flex items-center`}>
+              <span>
+                <Image
+                  src="https://static.plgworks.com/assets/images/non/wmatic-icon.png"
+                  alt="Lens Icon"
+                  width="26"
+                  height="26"
+                />
+              </span>
+              <span className="ml-[8px]">1 WMATIC</span>
+            </div>
+            <div className={`${styles.collectorCount} flex items-center`}>
+              <span>
+                <Image
+                  src="https://static.plgworks.com/assets/images/non/collectors.png"
+                  alt="Lens Icon"
+                  width="26"
+                  height="26"
+                />
+              </span>
+              <span className="ml-[12px]">{totalCollects} Collectors</span>
+            </div>
+            {modalData?.hasCollected || isNftCollectedByMe.current ? (
+              <button
+                className={`${styles.alreadyCollectedButton} flex items-center justify-center py-[7px]`}
+              >
+                <span>
+                  <Collect />
+                </span>
+                <span className="font-bold text-[16px] leading-[26px] ml-[8px]">
+                  You have already collected this
+                </span>
+              </button>
+            ) : (
+              <button
+                className={`${styles.collectButton} flex items-center justify-center py-[7px] mt-[20px]`}
+                onClick={() => collectPost()}
+              >
+                <span>
+                  <Collect />
+                </span>
+                {isLoading ? (
+                  <span className="pl-[11px]">Collecting...</span>
+                ) : isNftCollected ? (
+                  <span className="pl-[11px]">Collected</span>
+                ) : (
+                  <span className="pl-[11px]">Collect Now</span>
+                )}
+              </button>
+            )}{" "}
+          </>
         ) : null}
       </div>
     </div>
